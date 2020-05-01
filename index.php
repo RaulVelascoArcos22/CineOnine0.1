9f80487978d4f0f4734d2ed79f7502ca21915e6f
RaulVelascoArcos22 <velascoarcosraul@gmail.com>
2020-04-30 22:27:43 -0500

    Terminado

---
+++ index.php
@@ -0,0 +1,42 @@
+<!-- Velasco Arcos Raul -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/main.css">
+	<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet"> 
+	<title>Code Cine</title>
+</head>
+<body>
+	<header>
+		<div class="contenedor">
+			<h2 class="logotipo">CodeBit Cine</h2>
+			<nav>
+				<a href="#" class="activo">Inicio</a>
+				<a href="#">Programas</a>
+				<a href="#">Películas</a>
+				<a href="#">Más Recientes</a>
+                <a href="#">Mi lista</a>
+                <a href="#">Login</a>
+			</nav>
+		</div>
+	</header>
+
+	<main>
+		<div class="pelicula-principal">
+			<div class="contenedor">
+				<h3 class="titulo">Mala Educación</h3>
+				<p class="descripcion">
+                Un superintendente de un distrito escolar trabaja para mejorar la educación del estudiante mientras malversa fondos públicos para vivir la vida que desea.
+				</p>
+				<button role="button" class="boton"><i class="fas fa-play" onclick="window.location.href='https://www.youtube.com/watch?v=mrxN63Sh7_g'"></i>Reproducir</button>
+				<button role="button" class="boton"><i class="fas fa-info-circle"></i>Más información</button>
+			</div>
+		</div>
+	</main>
+	<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+    <script src="js/main.js"></script>
+    <script src="js/lity.js"></script>
+</body>
+</html>
